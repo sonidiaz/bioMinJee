6d966d2860bec0bb3d4bd728d47140489a7cb039
Ignacio Díaz <nachodiaz8@gmail.com>
2019-02-20 00:15:00 +0100

    Agregamos Trackingjs

--- assets/js/helpers/JeelizBioHelper.js
+++ assets/js/helpers/JeelizBioHelper.js
@@ -1,91 +1,6 @@
-'use strict';
 
-/**
- * Calculate the simple moving average of an array. A new array is returned with the average
- * of each range of elements. A range will only be calculated when it contains enough elements to fill the range.
- *
- * ```js
- * console.log(sma([1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
- * //=> [ '2.50', '3.50', '4.50', '5.50', '6.50', '7.50' ]
- * //=>   │       │       │       │       │       └─(6+7+8+9)/4
- * //=>   │       │       │       │       └─(5+6+7+8)/4
- * //=>   │       │       │       └─(4+5+6+7)/4
- * //=>   │       │       └─(3+4+5+6)/4
- * //=>   │       └─(2+3+4+5)/4
- * //=>   └─(1+2+3+4)/4
- * ```
- * @param  {Array} `arr` Array of numbers to calculate.
- * @param  {Number} `range` Size of the window to use to when calculating the average for each range. Defaults to array length.
- * @param  {Function} `format` Custom format function called on each calculated average. Defaults to `n.toFixed(2)`.
- * @return {Array} Resulting array of averages.
- * @api public
- */
+'use strict'
 
-function sma(arr, range, format) {
-  if (!Array.isArray(arr)) {
-    throw TypeError('expected first argument to be an array');
-  }
-
-  var fn = typeof format === 'function' ? format : toFixed;
-  var num = range || arr.length;
-  var res = [];
-  var len = arr.length + 1;
-  var idx = num - 1;
-  while (++idx < len) {
-    res.push(fn(avg(arr, idx, num)));
-  }
-  return res;
-}
-
-/**
- * Create an average for the specified range.
- *
- * ```js
- * console.log(avg([1, 2, 3, 4, 5, 6, 7, 8, 9], 5, 4));
- * //=> 3.5
- * ```
- * @param  {Array} `arr` Array to pull the range from.
- * @param  {Number} `idx` Index of element being calculated
- * @param  {Number} `range` Size of range to calculate.
- * @return {Number} Average of range.
- */
-
-function avg(arr, idx, range) {
-  return sum(arr.slice(idx - range, idx)) / range;
-}
-
-/**
- * Calculate the sum of an array.
- * @param  {Array} `arr` Array
- * @return {Number} Sum
- */
-
-function sum(arr) {
-  var len = arr.length;
-  var num = 0;
-  while (len--) num += Number(arr[len]);
-  return num;
-}
-
-/**
- * Default format method.
- * @param  {Number} `n` Number to format.
- * @return {String} Formatted number.
- */
-
-function toFixed(n) {
-  return n.toFixed(2);
-}
-
-/**
- * Expose `sma`
- */
-
-
-
-
-
-"use strict"
 
 const $head = document.getElementById('head');
 const $boca = document.getElementById('mouth');
@@ -131,11 +46,21 @@ const JeelizBioHelper = (function(){
         y:0,
         s:0
     }
+
+    let video,
+        canvasToImagen,
+        canvasToImagenCxt,
+        canvasReocorte,
+        canvasReocorteCtx,
+        videoDisplay;
+
     function callbackTrack(){
         console.log('object');
     }
 
     function callbackReady(errCode, spec){
+
+        
 
         if (errCode){
             console.log('ERROR in JeelizSVGHelper - CANNOT INITIALIZE JEEFACETRANSFERAPI : errCode =', errCode);
@@ -151,27 +76,96 @@ const JeelizBioHelper = (function(){
 
         // const canvasCut = document.querySelector('#cutface');
         // const canvasCxt = canvasCut.getContext('2d');
-        const video = document.querySelector('#cutFaceVideo');
+        video = document.querySelector('#cutFaceVideo');
+        canvasToImagen = window.canvasToImagen = document.querySelector('#canvasToImg');
+        canvasToImagenCxt = window.canvasToImagenCxt = canvasToImagen.getContext('2d');
+        
+        canvasReocorte  = window.canvasReocorte =  document.querySelector('#canvasToPicture');
+        canvasReocorteCtx = canvasReocorte.getContext('2d');
+        
+        canvasToImagen.width = JEEFACETRANSFERAPI.get_cv().width;
+        canvasToImagen.height = JEEFACETRANSFERAPI.get_cv().height;
+        
+
+
 
         canvasJeeliz = JEEFACETRANSFERAPI.get_cv();
 
-        
-        // CVD = JEEFACETRANSFERAPI.Canvas2DDisplay(spec);
-        // CVD.ctx.strokeStyle='yellow';
+        const container = document.querySelector('.demo-container');
 
-        // video.srcObject = this.spec.GL.canvas.captureStream();
-        // video.width = this.spec.GL.canvas.width;
-        // video.height = this.spec.GL.canvas.height;
-        
+     
+
+        let enviado = document.getElementById('img');
+        enviado.src = canvasToImagen.toDataURL('image/jpg');
+   
+        enviado.style.display = 'block';
+
+        takePhotoTracker();
+ 
         
     }
 
-    const coordinate = {
-        x:0,
-        y:0,
-        s:0
-    }
 
+    function takePhotoTracker(){
+            var img = document.getElementById('img');
+
+            var tracker = new tracking.LandmarksTracker();
+            tracker.setInitialScale(3);
+            tracker.setStepSize(1);
+            tracker.setEdgesDensity(0.1);
+
+            tracking.track('#videoToBio', tracker);
+
+            tracker.on('track', function(event) {
+                canvasToImagenCxt.clearRect(0,0,canvasToImagen.width, canvasToImagen.height);
+                if(!event.data) return;
+
+                event.data.faces.forEach(function(rect) {
+                    // window.plot(rect.x, rect.y, rect.width, rect.height);
+                    canvasReocorte.width = rect.width;
+                    canvasReocorte.height = rect.height;
+                    canvasToImagenCxt.strokeStyle = '#a64ceb';
+                    canvasToImagenCxt.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                    canvasToImagenCxt.font = '11px Helvetica';
+                    canvasToImagenCxt.fillStyle = "#fff";
+                    canvasToImagenCxt.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                    canvasToImagenCxt.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                    
+                    console.log(rect.x, rect.y, rect.width, rect.height);
+                    canvasReocorteCtx.drawImage(videoDisplay,rect.x, rect.y, rect.width, rect.height,0,0, rect.width, rect.height );
+                });
+
+
+                // event.data.landmarks.forEach(function(landmarks) {
+                // for(var i=0; i < landmarks.length; i++){
+                //     window.plotLandmark(landmarks[i][0], landmarks[i][1], 2, '#44ABDA');
+                // }
+                // });
+
+            });
+
+            window.plot = function(x, y, w, h) {
+                var rect = document.createElement('div');
+                document.querySelector('.demo-container').appendChild(rect);
+                rect.classList.add('rect');
+                rect.style.width = w + 'px';
+                rect.style.height = h + 'px';
+                rect.style.left = (img.offsetLeft + x) + 'px';
+                rect.style.top = (img.offsetTop + y) + 'px';
+            };
+
+            // window.plotLandmark = function(x,y, radius, color){
+            //     var circle = document.createElement('div');
+            //     document.querySelector('.demo-container').appendChild(circle);
+            //     circle.classList.add('circle');
+            //     circle.style.backgroundColor = color;
+            //     circle.style.width = (radius*2) + 'px';
+            //     circle.style.height = (radius*2) + 'px';
+            //     circle.style.left = (img.offsetLeft + x) + 'px';
+            //     circle.style.top = (img.offsetTop + y) + 'px';
+            // }
+
+    }
     function onMorphUpdate(quality, benchmarkCoeff){
 
         const box = document.querySelector('.box')
@@ -183,13 +177,11 @@ const JeelizBioHelper = (function(){
 
 
 
-        COORDINATES.x = window.innerWidth*(detectedState.x+1)/2;
-        COORDINATES.y = window.innerHeight*(detectedState.y+1)/2;
-        COORDINATES.w = window.innerWidth*detectedState.s;
+        // COORDINATES.x = window.innerWidth*(detectedState.x+1)/2;
+        // COORDINATES.y = window.innerHeight*(detectedState.y+1)/2;
+        // COORDINATES.w = window.innerWidth*detectedState.s;
         
         
-        console.log(COORDINATES.x, COORDINATES.y);
-
         // function getCoordinates(detectedState){
         //     COORDINATES.x=Math.round((0.5+0.5*detectedState.x*detectedState.s)*window.innerWidth);
         //     COORDINATES.y=Math.round((0.5+0.5*detectedState.y*detectedState.s)*window.innerHeight);
@@ -235,6 +227,7 @@ const JeelizBioHelper = (function(){
         if (_rotationCallback){
             _rotationCallback(_rotation);
         }
+        canvasToImagenCxt.drawImage(videoDisplay,0, 0, canvasToImagen.width, canvasToImagen.height,0,0, canvasToImagen.width, canvasToImagen.height );
     }
 
 
@@ -247,8 +240,7 @@ const JeelizBioHelper = (function(){
             // const { idealWidth, idealHeight } = spec.videoSettings;
       
             // const videoCanvas = document.querySelector('video');
-            const videoDisplay = document.querySelector('#videoToBio');
-            const canvasScreen = document.getElementById('screentest');
+            videoDisplay = document.querySelector('#videoToBio');
             
             _expressions=spec.expressions;
 
@@ -289,6 +281,7 @@ const JeelizBioHelper = (function(){
                 // videoInit.init();
             })
       
+            // canvasCxt.drawImage(video, ((faceCoo.x-(faceCoo.w*1.4-faceCoo.w)/2)), (faceCoo.y-((faceCoo.h*1.4-faceCoo.h)/2)), faceCoo.w*1.4, faceCoo.h*1.4, 400, 400, faceCoo.w, faceCoo.h);
 
         },
         detected: function(detected){
